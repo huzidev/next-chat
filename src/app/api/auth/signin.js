a4821389b1bc3b8a398521a69ce4cbe88a893372
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-12-29 13:13:03 +0500

    user signup api

---
+++ src/app/api/auth/signin.js
@@ -0,0 +1,12 @@
+import { auth } from "@/services/firebase";
+import { signInWithEmailAndPassword } from "firebase/auth";
+
+export default async function handler(req, res) {
+  const { email, password } = req.body;
+  try {
+    const response = await signInWithEmailAndPassword(auth, email, password);
+    return res.status(200).json({ uid: response.user.uid });
+  } catch (error) {
+    return res.status(400).json({ error: error.message });
+  }
+}
