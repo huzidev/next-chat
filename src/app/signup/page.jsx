18c018c129e585332a800b7257440eaeda9c0098
Huzaifa Iqbal <huzaifa.iqdev@gmail.com>
2024-12-29 13:35:19 +0500

    signup request

--- src/app/signup/page.jsx
+++ src/app/signup/page.jsx
@@ -12,10 +12,10 @@ export default function Page() {
     confirmPassword: "",
   });
   const [error, setError] = useState("");
+  const { username, email, password, confirmPassword } = user;
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-
     if (!username || !email || !password) {
       setError("Fields are required.");
     } else if (!/\S+@\S+\.\S+/.test(email)) {
@@ -25,7 +25,6 @@ export default function Page() {
     } else {
       await signup();
       setError("");
-      alert("Form submitted!");
     }
   };
 
@@ -39,22 +38,26 @@ export default function Page() {
 
   // Firebase sign up function
   async function signup() {
-    const { email, password, confirmPassword } = user;
     try {
-      const response = await createUserWithEmailAndPassword(
-        auth,
-        email,
-        password
-      );
-      console.log("User signed up: ", response);
-      // Add user data to your database
-      // await addUserToDatabase(response.user);
+      const response = await fetch("/api/auth/signup", {
+        method: "POST",
+        headers: { "Content-Type": "application/json" },
+        body: JSON.stringify({
+          email,
+          password,
+        }),
+      });
+      const data = await response.json();
+      if (response.ok) {
+        console.log("Successfully signed in:", data.uid);
+      } else {
+        setError(data.error);
+      }
     } catch (error) {
-      console.error("Error signing up: ", error);
+      console.error("Error signing up:", error);
+      setError("Something went wrong. Please try again.");
     }
   }
-
-  const { email, password, confirmPassword } = user;
 
   return (
     <main className="bg-[#26313c] h-screen flex justify-center items-center">
@@ -71,9 +74,10 @@ export default function Page() {
               className="mt-2 bg-gray-100 rounded-full border border-gray-300 px-4 py-2"
               type="username"
               id="username"
+              name="username"
               placeholder="Enter your username"
-              value={email}
-              onChange={handleChange}
+              value={username}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           <div>
@@ -84,9 +88,10 @@ export default function Page() {
               className="mt-2 bg-gray-100 rounded-full border border-gray-300 px-4 py-2"
               type="email"
               id="email"
+              name="email"
               placeholder="Enter your email"
               value={email}
-              onChange={handleChange}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           <div>
@@ -97,22 +102,27 @@ export default function Page() {
               className="mt-2 bg-gray-100 rounded-full border border-gray-300 px-4 py-2"
               type="password"
               id="password"
+              name="password"
               placeholder="Enter your password"
               value={password}
-              onChange={handleChange}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           <div>
-            <label htmlFor="password" className="font-medium text-gray-700">
+            <label
+              htmlFor="confirmPassword"
+              className="font-medium text-gray-700"
+            >
               Confirm Password
             </label>
             <Input
               className="mt-2 bg-gray-100 rounded-full border border-gray-300 px-4 py-2"
               type="password"
-              id="password"
+              name="confirmPassword"
+              id="confirmPassword"
               placeholder="Enter your password"
-              value={password}
-              onChange={handleChange}
+              value={confirmPassword}
+              onChange={(e) => handleChange(e)}
             />
           </div>
           {error && <p className="text-red-500 text-sm">{error}</p>}
